7bb27d48b825cca3a15db653f950160e3a464e21
koizuss <koizuss.apps@gmail.com>
2013-12-07 22:31:01 +0900

    yeoman適用

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global gain, $*/
+
+
+window.gain = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    gain.init();
+});
